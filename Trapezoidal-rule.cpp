22cebf422c3525f07c29a3b930998270fd72401b
N0t0p <sairoj25@gmail.com>
2023-02-14 19:13:44 +0545

    Using trapezoidal method

--- Trapezoidal-rule.cpp
+++ Trapezoidal-rule.cpp
@@ -11,11 +11,13 @@ int main(){
   cin>>b;
   cout<<"Enter the  stripes(n)"<<endl;
   cin>>n;
+
   h=(b-a)/n;
   sum1 = f(a)+f(b);
   for(i=0;i<=n-1;i++){
     sum2=sum2+f(a+i*h);
   }
   result=h/2 *(sum1+2*sum2);
+  
   cout<<"The result is "<<result<<endl;
 };
